1e8579ebdbd406e588ea7f6fce6fb64ad5fb03b2
keivo <zazitiskeivo@gmail.com>
2025-09-16 17:47:18 +0300

    main page

--- resources/views/login_register.blade.php
+++ resources/views/login_register.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Responsive Registration Form</title>
-  @vite(['resources/css/app.css'])
+  @vite(['resources/css/register.css'])
 </head>
 <body>
   <div class="container">
